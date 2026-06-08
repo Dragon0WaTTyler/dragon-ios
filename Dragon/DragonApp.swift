9948d881da3db2d17e829960520930ca7979a8a2
Amezziane <159654>
2026-06-08 03:19:43 +0100

    Initial Commit

---
+++ Dragon/DragonApp.swift
@@ -0,0 +1,17 @@
+//
+//  DragonApp.swift
+//  Dragon
+//
+//  Created by HH on 8/6/2026.
+//
+
+import SwiftUI
+
+@main
+struct DragonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
